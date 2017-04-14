0f2d36abf33335236ecb26a573d3d44e50ae7b5a
Matthias Hermann <matthias.h@online.de>
2017-04-14 14:43:34 +0200

    Add installation of basic system to script

--- install.sh
+++ install.sh
@@ -9,11 +9,14 @@
 #
 
 ## Use correct keyboard layout for my German laptop in case of user interaction
+echo "Loading keyboard layout '/usr/share/kbd/keymaps/i386/qwertz/de-mobii.map.gz'"
 loadskeys /usr/share/kbd/keymaps/i386/qwertz/de-mobii.map.gz
 
 
 
 ## Setup WiFi
+echo "Setting up WiFi connection..."
+echo
 ip link
 echo "Which WiFi interface do you want to use?"
 echo
@@ -35,3 +38,40 @@ dhcpcd ${WIFI_ADAPTER}
 
 # Alternatively 'wifi-menu' can be used
 # wifi-menu
+
+
+
+## Install the basic system
+echo "Installing the basic system"
+
+# Select appropriate mirrors
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+
+echo "Select the download mirrors you want to use with PACMAN."
+echo "Continue with 'Enter'"
+read
+nano /etc/pacman.d/mirrorlist
+
+# Install basic packages
+# package 'intel-ucode' is useful for machines running on intel chips
+# package 'wpa_supplicant' may be usefull to avoid WiFi problems during/after
+#	installation.
+echo "Installing basic packages..."
+pacstrap /mnt base base-devel wpa_supplicant intel-ucode
+
+# Generate file system table
+echo "Generating file system table..."
+genfstab -p /mnt > /mnt/etc/fstab
+
+echo "The generated file system table looks as follows:"
+echo
+cat /mnt/etc/fstab
+echo
+
+echo "NOTE: If you have installed Arch-Linux on a SSD, it is advised to change
+the file system table to mount '/' with options 'rw,defaults,noatime,discard'
+and mount 'swap' with options 'defaults,noatime,discard'."
+echo
+echo "Installation is now complete."
+echo "Entering operating system environment..."
+arch-chroot /mnt/
