de29ab20a162bf0a99c259e79c13646cdb27ffc1
Matthias Hermann <matthias.h@online.de>
2017-04-14 14:08:17 +0200

    Add template for install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# This script is intended to be used to install and configure an Arch Linux
+# automatically with minimal user interaction. Certain configurations are 
+# personal preference and may be adapted.
+#
+# The contents of this script are derived from the installation guide provided
+# by https://wiki.archlinux.de/title/Anleitung_f%C3%BCr_Einsteiger
+#
