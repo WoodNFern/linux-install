bbc4d3d1a3b73fb4d85d4d8fd5d6137d67a4127a
Matthias Hermann <matthias.h@online.de>
2017-04-16 18:20:51 +0200

    Add script for installing GRUB

---
+++ grub_install.sh
@@ -0,0 +1,10 @@
+## Install GRUB bootloader
+# Download and install GRUB
+echo "Installing GRUB bootloader..."
+pacman -S grub
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck --debug
+mkdir -p /boot/grub/locale
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+
+# Create grub configuration.
+grub-mkconfig -o /boot/grub/grub.cfg
