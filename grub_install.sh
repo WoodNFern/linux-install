b894d672d6fd8371612a52fe34c528d72d8ac8ab
Matthias Hermann <matthias.h@online.de>
2017-04-16 19:52:39 +0200

    Install UEFI tools before installing GRUB

--- grub_install.sh
+++ grub_install.sh
@@ -1,6 +1,10 @@
 ## Install GRUB bootloader
 # Download and install GRUB
 echo "Installing GRUB bootloader..."
+
+echo "Install tools for UEFI installation..."
+pacman -S efibootmgr dosfstools gptfdisk
+
 pacman -S grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck --debug
 mkdir -p /boot/grub/locale
