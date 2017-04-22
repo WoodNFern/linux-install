7128b02f1e024e9dbdc72e505ed37d5461c45d33
Matthias H <matthias.h@online.de>
2017-04-22 22:26:55 +0200

    Wrap sub-tasks in functions

--- 1_install.sh
+++ 1_install.sh
@@ -15,110 +15,125 @@ function echoSeperator {
 	echo
 }
 
-echo "Welcome to WoodNFern's installation script for Arch-Linux!"
-echo
-echo "Before running this installation, you have to partition your harddrive 
-according to your needs and create the filesystems."
-echo 
-while true; do
-    read -p "Have these steps been made? (y/N) " yn
-    case ${yn} in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) exit;;
-    esac
-done
-
 
 
 ## Check for clutter on EFI partition /mnt/boot
-if [ -e "/mnt/boot/vmlinuz-linux" ] || [ -e "/mnt/boot/intel-ucode.img"]
-then
-	echo "Detected residue files from previous installations in your EFI
-		partition."
-	echo "These files will cause problems during the installation."
-	echo "Please remove the following files, if present:"
-	echo "/mnt/boot/vmlinuz-linux"
-	echo "/mnt/boot/intel-ucode.img"
-	echo "Exiting installation now. You may resume after these files have
-		been deleted."
-	exit
-fi
-
-
-## Use correct keyboard layout for my German laptop in case of user interaction
-echo "Loading keyboard layout '/usr/share/kbd/keymaps/i386/qwertz/de-mobii.map.gz'"
-loadkeys /usr/share/kbd/keymaps/i386/qwertz/de-mobii.map.gz
+function detectGarbageBootFiles {
+	if [ -e "/mnt/boot/vmlinuz-linux" ] || [ -e "/mnt/boot/intel-ucode.img"]
+	then
+		echo "Detected residue files from previous installations in your EFI
+			partition."
+		echo "These files will cause problems during the installation."
+		echo "Please remove the following files, if present:"
+		echo "/mnt/boot/vmlinuz-linux"
+		echo "/mnt/boot/intel-ucode.img"
+		echo "Exiting installation now. You may resume after these files have
+			been deleted."
+		exit
+	fi
+}
 
 
 
-## Setup WiFi
-echo "Setting up WiFi connection..."
-echo
-ip link
-echo "Which WiFi interface do you want to use?"
-echo
-read WIFI_ADAPTER
-
-echo "What is the SSID of the network?"
-echo
-read SSID
-
-echo "What is the password for the network?"
-echo
-read -s WIFI_PASSWORD
-
-# Use provided logins to connect to the network with the specified WiFi adapter.
-# Selected device driver is 'wext' because of wider support.
-wpa_passphrase ${SSID} ${WIFI_PASSWORD} > /etc/wpa_supplicant/wpa_supplicant.conf
-wpa_supplicant -i ${WIFI_ADAPTER} -D wext -c /etc/wpa_supplicant/wpa_supplicant.conf -B
-dhcpcd ${WIFI_ADAPTER}
-
-# Alternatively 'wifi-menu' can be used
-# wifi-menu
+function printWelcomeMessage {
+	echo "Welcome to WoodNFern's installation script for Arch-Linux!"
+	echo
+	echo "Before running this installation, you have to partition your harddrive
+	according to your needs and create the filesystems."
+	echo
+	while true; do
+	    read -p "Have these steps been made? (y/N) " yn
+	    case ${yn} in
+		[Yy]* ) break;;
+		[Nn]* ) exit;;
+		* ) exit;;
+	    esac
+	done
+}
 
 
 
-## Install the basic system
-echo "Installing the basic system"
+function setupWifi {
+	## Setup WiFi
+	echo "Setting up WiFi connection..."
+	echo
+	ip link
+	echo "Which WiFi interface do you want to use?"
+	echo
+	read WIFI_ADAPTER
+
+	echo "What is the SSID of the network?"
+	echo
+	read SSID
+
+	echo "What is the password for the network?"
+	echo
+	read -s WIFI_PASSWORD
+
+	# Use provided logins to connect to the network with the specified WiFi adapter.
+	# Selected device driver is 'wext' because of wider support.
+	wpa_passphrase ${SSID} ${WIFI_PASSWORD} > /etc/wpa_supplicant/wpa_supplicant.conf
+	wpa_supplicant -i ${WIFI_ADAPTER} -D wext -c /etc/wpa_supplicant/wpa_supplicant.conf -B
+	dhcpcd ${WIFI_ADAPTER}
+
+	# Alternatively 'wifi-menu' can be used
+	# wifi-menu
+}
+
+
 
 # Select appropriate mirrors
 function setPacmanMirrors {
 	# Backup
 	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-	
+
 	# Uncomment all entries
 	sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.bak
-	
+
 	# Set 5 fastest mirrors automatically
 	rankmirrors -n 5 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist
 }
 
-setPacmanMirrors
 
 
 # Install basic packages
-# package 'intel-ucode' is useful for machines running on intel chips
-# package 'wpa_supplicant' may be usefull to avoid WiFi problems during/after
-#	installation.
-# package 'dialog' is needed to be able to use 'wifi-menu' after reboot for 
-#	easier WiFi setup.
-echo "Installing basic packages..."
-pacstrap /mnt base base-devel wpa_supplicant dialog intel-ucode
+function installBasicPackages {
+	# package 'intel-ucode' is useful for machines running on intel chips
+	# package 'wpa_supplicant' may be usefull to avoid WiFi problems during/after
+	#	installation.
+	# package 'dialog' is needed to be able to use 'wifi-menu' after reboot for
+	#	easier WiFi setup.
+	echo "Installing basic packages..."
+	pacstrap /mnt base base-devel wpa_supplicant dialog intel-ucode
 
-# Generate file system table
-echo "Generating file system table..."
-genfstab -Up /mnt > /mnt/etc/fstab
+	# Generate file system table
+	echo "Generating file system table..."
+	genfstab -Up /mnt > /mnt/etc/fstab
 
-echo "The generated file system table looks as follows:"
-echo
-cat /mnt/etc/fstab
-echo
+	echo "The generated file system table looks as follows:"
+	echo
+	cat /mnt/etc/fstab
+	echo
 
-echo "NOTE: If you have installed Arch-Linux on a SSD, it is advised to change
-the file system table to mount '/' with options 'rw,defaults,noatime,discard'
-and mount 'swap' with options 'defaults,noatime,discard'."
-echo
-echo "Installation of the basic system is now complete. Don't exit the script yet."
-echo "Entering operating system environment..."
-arch-chroot /mnt/
+	echo "NOTE: If you have installed Arch-Linux on a SSD, it is advised to change
+	the file system table to mount '/' with options 'rw,defaults,noatime,discard'
+	and mount 'swap' with options 'defaults,noatime,discard'."
+	echo
+	echo "Installation of the basic system is now complete. Don't exit the script yet."
+	echo "Entering operating system environment..."
+	arch-chroot /mnt/
+}
+
+## Use correct keyboard layout for my German laptop in case of user interaction
+echo "Loading keyboard layout '/usr/share/kbd/keymaps/i386/qwertz/de-mobii.map.gz'"
+loadkeys /usr/share/kbd/keymaps/i386/qwertz/de-mobii.map.gz
+
+detectGarbageBootFiles
+
+printWelcomeMessage
+
+setupWifi
+
+setPacmanMirrors
+
+installBasicPackages
--- 3_init_system.sh
+++ 3_init_system.sh
@@ -15,76 +15,85 @@ function echoSeperator {
 	echo
 }
 
-# Reestablish network connection
-wifi-menu
 
-# Add a non-root user
-echo "Adding a new user..."
-echo "Please enter the name of the user (small letters only)"
-read USERNAME
-useradd -m -g users -s /bin/bash ${USERNAME}
-passwd ${USERNAME}
 
-# Enable sudo
-echo "Enabling 'sudo' functionality..."
-echo
-pacman -S --noconfirm sudo
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+function addSudoUser {
+	# Add a non-root user
+	echo "Adding a new user..."
+	echo "Please enter the name of the user (small letters only)"
+	read USERNAME
+	useradd -m -g users -s /bin/bash ${USERNAME}
+	passwd ${USERNAME}
 
-# Add user to important groups
-echo "Add user to important groups..."
-gpasswd -a ${USERNAME} wheel
-gpasswd -a ${USERNAME} audio
-gpasswd -a ${USERNAME} video
-gpasswd -a ${USERNAME} games
-gpasswd -a ${USERNAME} power
+	# Enable sudo
+	echo "Enabling 'sudo' functionality..."
+	echo
+	pacman -S --noconfirm sudo
+	echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
-# Install  and enable additional services
-echo "Install and enable useful services..."
-echo
-pacman -S --noconfirm acpid ntp dbus avahi cups cronie networkmanager
-systemctl enable acpid
-systemctl enable ntpd
-systemctl enable avahi-daemon
-systemctl enable org.cups.cupsd.service
-systemctl enable NetworkManager.service
-systemctl enable cronie
-
-# Set up NTP
-echo "Installing NTP..."
-echo
-pacman -S --noconfirm ntp
-echo "You may now enter a different time server (e.g. de.pool.ntp.org)"
-echo "Press 'Enter' to continue"
-read
-nano /etc/ntp.conf
-
-ntpd -gq
-echo "The current time / date is:"
-date
-hwclock -w
+	# Add user to important groups
+	echo "Add user to important groups..."
+	gpasswd -a ${USERNAME} wheel
+	gpasswd -a ${USERNAME} audio
+	gpasswd -a ${USERNAME} video
+	gpasswd -a ${USERNAME} games
+	gpasswd -a ${USERNAME} power
+}
 
 
 
-## Install X and other components
-echo "Installing necessary X components"
-pacman -S --noconfirm xorg-server xorg-xinit xorg-utils xorg-server-utils
-
-echo "Installing device driver for Intel chips..."
-pacman -S --noconfirm xf86-video-intel
-
-echo "Installing touchpad driver..."
-pacman -S --noconfirm xf86-input-synaptics
-
-echo "Setting keymap..."
-localectl set-x11-keymap de acer_laptop de_nodeadkeys
-
-echo "Installing nice fonts..."
-pacman -S --noconfirm ttf-dejavu ttf-inconsolata ttf-liberation ttf-symbola terminus-font
+function installMiscServices {
+	# Install  and enable additional services
+	echo "Install and enable useful services..."
+	echo
+	pacman -S --noconfirm acpid ntp dbus avahi cups cronie networkmanager
+	systemctl enable acpid
+	systemctl enable ntpd
+	systemctl enable avahi-daemon
+	systemctl enable org.cups.cupsd.service
+	systemctl enable NetworkManager.service
+	systemctl enable cronie
+}
 
 
 
-## Install window manager
+function setupNTP {
+	# Set up NTP
+	echo "Installing NTP..."
+	echo
+	pacman -S --noconfirm ntp
+	echo "You may now enter a different time server (e.g. de.pool.ntp.org)"
+	echo "Press 'Enter' to continue"
+	read
+	nano /etc/ntp.conf
+
+	ntpd -gq
+	echo "The current time / date is:"
+	date
+	hwclock -w
+}
+
+
+
+function installXorgComponents {
+	## Install X and other components
+	echo "Installing necessary X components"
+	pacman -S --noconfirm xorg-server xorg-xinit xorg-utils xorg-server-utils
+
+	echo "Installing device driver for Intel chips..."
+	pacman -S --noconfirm xf86-video-intel
+
+	echo "Installing touchpad driver..."
+	pacman -S --noconfirm xf86-input-synaptics
+
+	echo "Setting keymap..."
+	localectl set-x11-keymap de acer_laptop de_nodeadkeys
+
+	echo "Installing nice fonts..."
+	pacman -S --noconfirm ttf-dejavu ttf-inconsolata ttf-liberation ttf-symbola terminus-font
+}
+
+
 
 function install_gnome {
 	pacman -S --noconfirm gnome gnome-extra
@@ -101,36 +110,55 @@ function install_xfce4 {
 	systemctl enable slim
 }
 
-PS3='Which window manager do you want to install? '
-options=("GNOME" "Xfce4")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "GNOME")
-            echo "Installing GNOME..."
-			install_gnome
-			break
-            ;;
-        "Xfce4")
-            echo "Installing Xfce4..."
-			install_xfce4
-			break
-            ;;        
-        *) echo "Invalid option";;
-    esac
-done
+function installWindowManager {
+	PS3='Which window manager do you want to install? '
+	options=("GNOME" "Xfce4")
+	select opt in "${options[@]}"
+	do
+	    case $opt in
+		"GNOME")
+		    echo "Installing GNOME..."
+				install_gnome
+				break
+		    ;;
+		"Xfce4")
+		    echo "Installing Xfce4..."
+				install_xfce4
+				break
+		    ;;
+		*) echo "Invalid option";;
+	    esac
+	done
+}
 
 
 
-## Install ALSA
-echo "Installing ALSA (Advanced Linux Sound Architecture)..."
-pacman -S --noconfirm alsa-utils
+function installALSA {
+	echo "Installing ALSA (Advanced Linux Sound Architecture)..."
+	pacman -S --noconfirm alsa-utils
+}
 
 
 
-## MISC
+function installACPISupport {
+	pacman -S --noconfirm acpid
+	sudo systemctl enable acpid
+	sudo systemctl start acpid
+}
 
-# ACPI support for notebooks
-pacman -S --noconfirm acpid
-sudo systemctl enable acpid
-sudo systemctl start acpid
+# Reestablish network connection
+wifi-menu
+
+addSudoUser
+
+installMiscServices
+
+setupNTP
+
+installXorgComponents
+
+installWindowManager
+
+installALSA
+
+installACPISupport
